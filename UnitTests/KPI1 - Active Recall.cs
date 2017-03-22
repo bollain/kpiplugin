71eb719ed99d14f9ca3f02ca8cce445188cbf9af
Curtis Moody <CZMoodyDev@gmail.com>
2017-03-21 19:10:36 -0700

    Added almost all my work...reverted csproj to I think...

--- UnitTests/KPI1 - Active Recall.cs
+++ UnitTests/KPI1 - Active Recall.cs
@@ -3,6 +3,8 @@ using System.IO;
 using System.Data;
 using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenDental;
+using KPIReporting.KPI;
 using OpenDentBusiness;
 
 
@@ -11,45 +13,20 @@ namespace UnitTests
     [TestClass]
     public class KPI1
     {
-        [TestMethod]
-        public void InjectPatients()
+        [TestInitialize]
+        public void Initialize()
         {
-            string injectKPI = @"/* Truncate all involved tables */
-            TRUNCATE TABLE unittest.patient;
-            TRUNCATE TABLE unittest.recall;
-            TRUNCATE TABLE unittest.procedurelog;
+            //Create test DB
+            DatabaseTools.SetDbConnection("", "localhost", "3306", "root", "", false);
+            DatabaseTools.FreshFromDump("localhost", "3306", "root", "", false);
+            //Inject data
+            var seedData = File.ReadAllText(@"..\..\Resources\kpi1activerecall.txt");
+            DatabaseTools.ExecuteSqlScript("localhost", "3306", "root", "", seedData);
+        }
 
-            /* Insert 1 patient who should qualify as on Active Recall */
-            INSERT INTO `unittest`.`procedurelog` (`PatNum`, `CodeNum`, `ProcDate`) VALUES('1', '404', '2016-12-25');
-            INSERT INTO `unittest`.`recall` (`PatNum`, `RecallInterval`, `IsDisabled`, `RecallTypeNum`) VALUES('1', '16777216', '0', '1');
-            INSERT INTO `unittest`.`patient` (`LName`, `FName`, `MiddleI`, `Gender`, `Birthdate`, `Zip`, `PriProv`) VALUES('Test', 'Patient', '1', '1', '1990-07-21', 'T6E1R1', '1');
-
-            /*Amanda TooLongAgo - Patient with latest procedure date too far in the past to qualify for being on active recall*/
-            INSERT INTO `unittest`.`patient` (`LName`, `FName`, `Gender`, `Birthdate`, `Zip`, `PriProv`) VALUES('TooLongAgo', 'Amanda', '1', '1976-03-22', 'A5A5A5', '1');
-            INSERT INTO `unittest`.`procedurelog` (`PatNum`, `CodeNum`, `ProcDate`) VALUES('2', '404', '2013-12-25');
-            INSERT INTO `unittest`.`recall` (`PatNum`, `RecallInterval`, `IsDisabled`, `RecallTypeNum`) VALUES('2', '167772', '0', '1');
-
-            /*Tom IsDisabled - Patient with recall flagged as disabled */
-            INSERT INTO `unittest`.`patient` (`LName`, `FName`, `Gender`, `Birthdate`, `Zip`, `PriProv`) VALUES('IsDisabled', 'Tom', '0', '1982-06-28', 'T6E1R1', '1');
-            INSERT INTO `unittest`.`procedurelog` (`PatNum`, `CodeNum`, `ProcDate`) VALUES('3', '404', '2017-12-25');
-            INSERT INTO `unittest`.`recall` (`PatNum`, `RecallInterval`, `IsDisabled`, `RecallTypeNum`) VALUES('3', '167772', '1', '1');
-
-            /*Mary WrongCode - Patient with procedure code that is not 404 */
-            INSERT INTO `unittest`.`patient` (`LName`, `FName`, `Gender`, `Birthdate`, `Zip`, `PriProv`) VALUES('WrongCode', 'Mary', '1', '1999-10-28', 'T6E1R1', '1');
-            INSERT INTO `unittest`.`procedurelog` (`PatNum`, `CodeNum`, `ProcDate`) VALUES('4', '30000', '2017-12-25');
-            INSERT INTO `unittest`.`recall` (`PatNum`, `RecallInterval`, `IsDisabled`, `RecallTypeNum`) VALUES('4', '167772', '0', '1');
-
-            /*Marsha NoRecall - Patient with no table instance of recall */
-            INSERT INTO `unittest`.`patient` (`LName`, `FName`, `Gender`, `Birthdate`, `Zip`, `PriProv`) VALUES('NoRecall', 'Marsha', '1', '1995-03-28', 'T6E1R1', '1');
-            INSERT INTO `unittest`.`procedurelog` (`PatNum`, `CodeNum`, `ProcDate`) VALUES('5', '404', '2017-12-25');
-
-            /*Erica NoLog- Patient with no procedurelog at all */
-            INSERT INTO `unittest`.`patient` (`LName`, `FName`, `Gender`, `Birthdate`, `Zip`, `PriProv`) VALUES('NoLog', 'Erica', '1', '1959-10-28', 'T6E1R1', '1');
-            INSERT INTO `unittest`.`recall` (`PatNum`, `RecallInterval`, `IsDisabled`, `RecallTypeNum`) VALUES ('6', '167772', '0', '1');";
-
-            //If the above gives an SQL syntax error, strip all white linebreaks
-            injectSQLWithSpecialGuestArmando(injectKPI);
-
+        [TestMethod]
+        public void GetPatientsOnActiveRecall()
+        {
             DataTable real_dt = KPIActiveRecall.GetActiveRecall(Convert.ToDateTime("2016-03-20"), Convert.ToDateTime("2017-03-20"));
             DataTable expected_dt = new DataTable();
 
@@ -65,11 +42,20 @@ namespace UnitTests
             DataRow _testPat = expected_dt.NewRow();
             _testPat["Name"] = "Test, Patient 1";
             _testPat["Gender"] = "F";
-            _testPat["Age"] = "26";
+            var birthdate = Convert.ToDateTime("1990-07-21");
+            var age = DateTime.UtcNow.Year - birthdate.Year;
+            if (birthdate > DateTime.UtcNow.AddYears(-age)) age--;
+            _testPat["Age"] = age.ToString();
             _testPat["Postal Code"] = "T6E1R1";
             _testPat["Date of Service"] = "25/12/2016";
             _testPat["Frequency"] = "1y";
             _testPat["Primary Provider"] = "DOC";
+
+            expected_dt.Rows.Add(_testPat);
+
+            Assert.IsNotNull(real_dt);
+            Assert.AreEqual(1, real_dt.Rows.Count);
+            Assert.AreEqual(real_dt.Rows[0]["Name"], expected_dt.Rows[0]["Name"]);
 
         }
     }
