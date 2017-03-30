043f833341c56aaa502c0c67e8a8ab3d9194851f
trigorob <trigomusic@hotmail.com>
2017-03-29 17:31:12 -0700

    Pushing KPIs 13 and 14... (Note: KPI13 and 14 handlers depend on StretchKPICustomForm)

---
+++ KPI/KPIPendingTreatments.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace OpenDentBusiness {
+    public class KPIPendingTreatments
+    {
+
+        ///<summary>If not using clinics then supply an empty list of clinicNums. dateStart and dateEnd can be MinVal/MaxVal to indicate "forever".</summary>
+        public static DataTable GetPendingTreatments(DateTime dateStart, DateTime dateEnd)
+        {
+            if (RemotingClient.RemotingRole == RemotingRole.ClientWeb)
+            {
+                return Meth.GetTable(MethodBase.GetCurrentMethod(), dateStart, dateEnd);
+            }
+            DataTable table = new DataTable();
+            table.Columns.Add("PatNum");
+            table.Columns.Add("Name");
+            table.Columns.Add("Home Phone");
+            table.Columns.Add("Work Phone");
+            table.Columns.Add("Wireless Phone");
+            table.Columns.Add("Email");
+            table.Columns.Add("Procedure Code");
+            table.Columns.Add("Treatment Planned");
+
+
+            DataRow row;
+            /*
+            13.Pending treatment(patients waiting for appointment)
+            List of all patients whose dental treatment has not been scheduled.This would not include
+            procedure code 01202.Extract the information in Chart module/ Planned Appointment tab
+            at the top right side of the screen – Report should extract for all patients with entries in this
+            section of the chart - patient’s name and ID#, contact information, and procedures planned
+            (will include procedure code and description).Note: If the treatment has been scheduled,
+            there should not be an entry in the “Planned Appointment” section of the chart.
+
+            */
+
+            string command = @"
+				SELECT p.PatNum, p.LName, p.FName, p.MiddleI, p.Gender, p.Zip, p.PriProv, 
+           p.HmPhone, p.WkPhone, p.WirelessPhone, p.Email, pc.Descript, pc.ProcCode 
+FROM procedurelog pl
+JOIN procedurecode pc ON pl.CodeNum = pc.CodeNum
+JOIN appointment a ON a.AptNum = pl.PlannedAptNum
+JOIN patient p ON a.PatNum = p.PatNum
+WHERE pl.AptNum = 0
+AND a.AptStatus = 6
+AND pc.ProcCode != 01202
+";
+
+
+            /*
+				SELECT p.PatNum, p.LName, p.FName, p.MiddleI, p.Gender, p.Zip, p.PriProv, 
+           p.HmPhone, p.WkPhone, p.WirelessPhone, p.Email, pc.Descript, pc.ProcCode 
+FROM plannedappt pla
+JOIN appointment a ON a.AptNum = pla.AptNum
+JOIN patient p ON a.PatNum = p.PatNum
+JOIN procedurelog pl ON pl.PlannedAptNum = pla.AptNum
+JOIN procedurecode pc ON pc.CodeNum = pl.CodeNum
+WHERE pc.ProcCode != 01202              
+             
+             FROM opendental.plannedappt pla
+JOIN opendental.appointment a ON a.AptNum = pla.AptNum
+JOIN opendental.patient p ON a.PatNum = p.PatNum 
+JOIN opendental.procedurelog pl ON pl.PlannedAptNum = pla.AptNum
+JOIN opendental.procedurecode pc ON pc.CodeNum = pl.CodeNum
+WHERE pc.ProcCode != 01202
+
+              
+              SELECT p.LName, p.FName, p.MiddleI, p.Gender, p.Zip, p.PriProv, p.Preferred, r.AptDateTime, p.Birthdate, r.ProcDescript 
+FROM patient p 
+JOIN appointment r ON r.PatNum = p.PatNum 
+WHERE r.AptDateTime = (
+	SELECT MAX(r2.AptDateTime) 
+	FROM appointment r2
+	WHERE r.PatNum = r2.PatNum 
+		AND r2.AptStatus = '3'
+		## AND r2.CodeNum = 01202 
+             */
+
+
+            DataTable raw = ReportsComplex.GetTable(command);
+            Patient pat;
+            for (int i = 0; i < raw.Rows.Count; i++)
+            {
+                row = table.NewRow();
+                pat = new Patient();
+                // pat.PatNum = (long)raw.Rows[i]["PatNum"];
+                pat.LName = raw.Rows[i]["LName"].ToString();
+                pat.FName = raw.Rows[i]["FName"].ToString();
+                pat.MiddleI = raw.Rows[i]["MiddleI"].ToString();
+                // pat.Preferred = raw.Rows[i]["Preferred"].ToString();
+                row["Name"] = pat.GetNameLF();
+                pat.HmPhone = raw.Rows[i]["HmPhone"].ToString();
+                pat.WkPhone = raw.Rows[i]["WkPhone"].ToString();
+                pat.WirelessPhone  = raw.Rows[i]["WirelessPhone"].ToString();
+                pat.Email = raw.Rows[i]["Email"].ToString();
+
+                row["Home Phone"] = raw.Rows[i]["HmPhone"].ToString();
+                row["Work Phone"] = raw.Rows[i]["WkPhone"].ToString();
+                row["Wireless Phone"] = raw.Rows[i]["WirelessPhone"].ToString();
+                row["Email"] = raw.Rows[i]["Email"].ToString();
+
+                row["Procedure Code"] = raw.Rows[i]["ProcCode"].ToString();
+                row["Treatment Planned"] = raw.Rows[i]["Descript"].ToString();
+
+                // row["Primary Provider"] = Providers.GetAbbr(PIn.Long(raw.Rows[i]["PriProv"].ToString()));
+                // row["Sex"] = raw.Rows[i]["Gender"].ToString();
+                // row["Postal Code"] = raw.Rows[i]["Zip"].ToString();
+                // row["Date of Service"] = raw.Rows[i]["AptDateTime"].ToString().Substring(0, 10);
+                // row["Appointment Description"] = raw.Rows[i]["ProcDescript"].ToString();
+                // row["Age"] = birthdate_to_age(raw.Rows[i]["Birthdate"].ToString()); 
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        private static string genderFormat(string gNum)
+        {
+            if (gNum == "0")
+            {
+                return "M";
+            }
+            else if (gNum == "1")
+            {
+                return "F";
+            }
+            else
+            {
+                return "Unknown";
+            }
+        }
+
+        public static DataTable GetPendingTreatmentPats(DateTime dateStart, DateTime dateEnd)
+        {
+            if (RemotingClient.RemotingRole == RemotingRole.ClientWeb)
+            {
+                return Meth.GetTable(MethodBase.GetCurrentMethod(), dateStart, dateEnd);
+            }
+            DataTable table = new DataTable();
+            table.Columns.Add("PatNum");
+            table.Columns.Add("Name");
+            table.Columns.Add("Home Phone");
+            table.Columns.Add("Cell Phone");
+            table.Columns.Add("Wireless Phone");
+            table.Columns.Add("Email");
+            // table.Columns.Add("Procedure Code");
+            // table.Columns.Add("Treatment Planned");
+
+
+            DataRow row;
+
+            string command = @"
+				SELECT DISTINCT p.PatNum, p.LName, p.FName, p.MiddleI, 
+                           p.HmPhone, p.WkPhone, p.WirelessPhone, p.Email
+                FROM procedurelog pl
+                JOIN procedurecode pc ON pl.CodeNum = pc.CodeNum
+                JOIN appointment a ON a.AptNum = pl.PlannedAptNum
+                JOIN patient p ON a.PatNum = p.PatNum
+                WHERE pl.AptNum = 0
+                AND a.AptStatus = 6
+                AND pc.ProcCode != 01202
+            ";
+
+            DataTable raw = ReportsComplex.GetTable(command);
+            Patient pat;
+            for (int i = 0; i < raw.Rows.Count; i++)
+            {
+                row = table.NewRow();
+                pat = new Patient();
+                pat.LName = raw.Rows[i]["LName"].ToString();
+                pat.FName = raw.Rows[i]["FName"].ToString();
+                pat.MiddleI = raw.Rows[i]["MiddleI"].ToString();
+                // row["Name"]=pat.GetNameLF();
+                row["Name"] = raw.Rows[i]["FName"].ToString() + " " + raw.Rows[i]["MiddleI"].ToString() + 
+                ". " + raw.Rows[i]["LName"].ToString();
+                pat.HmPhone = raw.Rows[i]["HmPhone"].ToString();
+                pat.WkPhone = raw.Rows[i]["WkPhone"].ToString();
+                pat.WirelessPhone = raw.Rows[i]["WirelessPhone"].ToString();
+                pat.Email = raw.Rows[i]["Email"].ToString();
+
+                row["PatNum"] = raw.Rows[i]["PatNum"];
+                row["Home Phone"] = raw.Rows[i]["HmPhone"].ToString();
+                row["Work Phone"] = raw.Rows[i]["WkPhone"].ToString();
+                row["Cell Phone"] = raw.Rows[i]["WirelessPhone"].ToString();
+                row["Email"] = raw.Rows[i]["Email"].ToString();
+
+                table.Rows.Add(row);
+            }
+
+
+            return table;
+
+        }
+
+        public static DataTable GetPendingTreatmentProcsPerPat(DateTime dateStart, DateTime dateEnd, String patNum)
+        {
+            if (RemotingClient.RemotingRole == RemotingRole.ClientWeb)
+            {
+                return Meth.GetTable(MethodBase.GetCurrentMethod(), dateStart, dateEnd);
+            }
+            DataTable table = new DataTable();
+            table.Columns.Add("Procedure Code");
+            table.Columns.Add("Treatment Planned");
+
+
+            DataRow row;
+
+            string command = @"
+				SELECT  pc.Descript, pc.ProcCode 
+                FROM procedurelog pl
+                JOIN procedurecode pc ON pl.CodeNum = pc.CodeNum
+                JOIN appointment a ON a.AptNum = pl.PlannedAptNum
+                JOIN patient p ON a.PatNum = p.PatNum
+                WHERE pl.AptNum = 0
+                AND a.AptStatus = 6
+                AND pc.ProcCode != 01202
+                AND p.PatNum = '" + patNum + @"'
+            ";
+
+            DataTable raw = ReportsComplex.GetTable(command);
+            // Patient pat;
+            for (int i = 0; i < raw.Rows.Count; i++)
+            {
+                row = table.NewRow();
+                row["Procedure Code"] = raw.Rows[i]["ProcCode"].ToString();
+                row["Treatment Planned"] = raw.Rows[i]["Descript"].ToString();
+
+                table.Rows.Add(row);
+            }
+
+
+            return table;
+
+        }
+
+
+        private static int birthdate_to_age(string bd)
+        {
+            DateTime birthdate = Convert.ToDateTime(bd);
+            var today = DateTime.UtcNow;
+            var age = today.Year - birthdate.Year;
+            if (birthdate > today.AddYears(-age)) age--;
+            return age;
+        }
+
+    }
+}
---
+++ KPIForm/FormKPICompletedCases.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using OpenDentBusiness;
+using OpenDental.ReportingComplex;
+
+namespace OpenDental.KPI_Reports
+{
+    public partial class FormKPICompletedCases : Form
+    {
+
+        public FormKPICompletedCases()
+        {
+            InitializeComponent();
+            Lan.F(this);
+        }
+
+        private void FormKPICompletedCases_Load(object sender, EventArgs e)
+        {
+            dateStart.SelectionStart = DateTime.Today;
+            dateEnd.SelectionStart = DateTime.Today.AddYears(1);
+        }
+
+        private void butOK_Click(object sender, EventArgs e)
+        {
+            ReportComplex report = new ReportComplex(true, false);
+            report.ReportName = Lan.g(this, "Completed Cases");
+            report.AddTitle("Title", Lan.g(this, "Completed Cases"));
+            report.AddSubTitle("Date", dateStart.SelectionStart.ToShortDateString() + " - " + dateEnd.SelectionStart.ToShortDateString());
+
+            DataTable tablePats;
+            // tablePats = KPICompletedCases.GetCompletedCases(dateStart.SelectionStart, dateEnd.SelectionStart);
+            String patQuery = @"
+				SELECT DISTINCT p.PatNum, p.LName, p.FName, p.MiddleI, 
+                           p.HmPhone, p.WkPhone, p.WirelessPhone, p.Email
+                FROM opendental.procedurelog pl
+                JOIN opendental.appointment a ON pl.PlannedAptNum = a.AptNum
+                JOIN opendental.procedurecode pc ON pc.CodeNum = pl.CodeNum
+                JOIN opendental.patient p ON p.PatNum = pl.PatNum
+                WHERE a.AptStatus = 6
+                AND pl.ProcStatus = 2            
+            ";
+
+
+           // tablePats = StretchKPICustomForm.GetPatients(dateStart.SelectionStart, dateEnd.SelectionStart, patQuery);
+            tablePats = KPICompletedCases.GetCompletedCasesPats(dateStart.SelectionStart, dateEnd.SelectionStart);
+            for (int i = 0; i < tablePats.Rows.Count; i++)
+            {
+                DataTable onePat = new DataTable();
+                DataTable localPat = tablePats.Clone();
+
+
+                DataRow iPat = tablePats.Rows[i];
+                //onePat.ImportRow(iPat);
+                // localPat.Clear();
+                localPat.ImportRow(iPat);
+
+                QueryObject query = report.AddQuery(localPat, "", "", SplitByKind.None, 0);
+                // query.AddColumn("PatNum", 90, FieldValueType.Number);
+                query.AddColumn("PatNum", 60, FieldValueType.String);
+                query.AddColumn("Name", 150, FieldValueType.String);
+                query.AddColumn("Home Phone", 80, FieldValueType.String);
+                query.AddColumn("Work Phone", 80, FieldValueType.String);
+                query.AddColumn("Cell Phone", 80, FieldValueType.String);
+                query.AddColumn("Email", 150, FieldValueType.String);
+
+                String iPatNum = iPat["PatNum"].ToString();
+
+                DataTable procsForPat = KPICompletedCases.GetCompletedCasesPerPat(dateStart.SelectionStart,
+                    dateEnd.SelectionStart, iPatNum);
+
+                QueryObject procsQ = report.AddQuery(procsForPat, "", "", SplitByKind.None, 0);
+                procsQ.AddColumn("Date of Service", 100, FieldValueType.String);
+                procsQ.AddColumn("Treatment Code", 100, FieldValueType.String);
+                procsQ.AddColumn("Treatment Completed", 350, FieldValueType.String);
+                procsQ.AddColumn("Billed", 50, FieldValueType.Number);
+
+            //    procsQ.AddGroupSummaryField("Number of Completed Cases for "+ iPat["Name"] + ":", "Date of Service", 
+            //        "Treatment Code", SummaryOperation.Count);
+            //    procsQ.AddGroupSummaryField("TOTAL:", "Billed", "Billed", SummaryOperation.Count);
+
+
+                report.AddLine("line", AreaSectionType.GroupFooter, Color.AliceBlue, 20,
+                    LineOrientation.Horizontal, LinePosition.Center, 25, 0, 0);
+
+            }
+
+            /*
+            // OLDER FORM:
+            DataTable tablePats = KPICompletedCases.GetCompletedCases(dateStart.SelectionStart, dateEnd.SelectionStart);
+            ReportComplex report = new ReportComplex(true, false);            
+            report.ReportName = Lan.g(this, "Completed Cases");
+            report.AddTitle("Title", Lan.g(this, "Completed Cases"));
+            report.AddSubTitle("Date", dateStart.SelectionStart.ToShortDateString() + " - " + dateEnd.SelectionStart.ToShortDateString());
+            QueryObject query;
+            query = report.AddQuery(tablePats, "", "", SplitByKind.None, 0);
+            query.AddColumn("Name", 150, FieldValueType.String);
+
+            query.AddColumn("Date of Service", 100, FieldValueType.String);
+            query.AddColumn("Treatment Code", 80, FieldValueType.String);
+            query.AddColumn("Treatment Completed", 280, FieldValueType.String);
+            query.AddColumn("Billed", 50, FieldValueType.String);
+            //        query.AddColumn("Birthdate", 80, FieldValueType.String);
+            //        query.AddColumn("Sex", 150, FieldValueType.String);
+            //        query.AddColumn("Postal Code", 90, FieldValueType.String);
+            //        query.AddColumn("Date of Service", 100, FieldValueType.String);
+            //        query.AddColumn("Primary Provider", 40, FieldValueType.String);
+            //        query.AddGroupSummaryField("Number of Completed Cases:", "Name", "Provider", SummaryOperation.Count);
+
+            */
+            report.AddPageNum();
+            if (!report.SubmitQueries())
+            {
+                return;
+            }
+            FormReportComplex FormR = new FormReportComplex(report);
+            FormR.ShowDialog();
+            //DialogResult=DialogResult.OK;		
+        }
+
+        private void butCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+    }
+}
