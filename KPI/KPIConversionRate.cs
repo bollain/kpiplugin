9ff88bf8fcda6961230f574f05ba01c737911b55
bmteo <bmteo@users.noreply.github.com>
2017-03-29 23:40:37 -0700

    Update KPIConversionRate.cs

--- KPI/KPIConversionRate.cs
+++ KPI/KPIConversionRate.cs
@@ -7,13 +7,13 @@ using OpenDental;
 
 namespace KPIReporting.KPI
 {
-    public class KPIConversionRate
+     public class KPIConversionRate
     {
 
         ///<summary>If not using clinics then supply an empty list of clinicNums. dateStart and dateEnd can be MinVal/MaxVal to indicate "forever".</summary>
         public static float GetConversionRate(DateTime dateStart, DateTime dateEnd, String pc)
         {
-           
+           // Find all procedures for that ProcCode
             string command = @"
 
                 SELECT COUNT(*)
@@ -22,7 +22,7 @@ namespace KPIReporting.KPI
                 JOIN procedurecode x ON x.CodeNum = pl.CodeNum 
                 WHERE x.ProcCode = " + "'" + POut.String(pc) + "'" + @" 
                     AND pl.DateTP BETWEEN " + POut.DateT(dateStart) + @" AND " + POut.DateT(dateEnd) + @"";
-
+            // Find procedures that have been planned.
             string command2 = @"
                 SELECT COUNT(*)
                 FROM appointment a
@@ -36,15 +36,27 @@ namespace KPIReporting.KPI
             DataTable raw = ReportsComplex.GetTable(command);
             DataTable raw2 = ReportsComplex.GetTable(command2);
             float returnval = 0;
+            // No Procedures At All
+           
+               
+                float TOTALPROCEDURES = float.Parse(raw.Rows[0]["COUNT(*)"].ToString());
+                float PROCEDURESPLANNED = float.Parse(raw2.Rows[0]["COUNT(*)"].ToString());
+                float TOTALCOUNTPROCEDURES = TOTALPROCEDURES + PROCEDURESPLANNED;
+            if (TOTALCOUNTPROCEDURES == 0)
+            {
+                return 9999;
+            }
+            else { 
+            System.Diagnostics.Debug.Write(TOTALCOUNTPROCEDURES);
+                System.Diagnostics.Debug.Write(PROCEDURESPLANNED);
 
-            float TOTALCOUNTPROCEDURES = float.Parse(raw.Rows[0]["COUNT(*)"].ToString());
-            float PROCEDURESPLANNED = float.Parse(raw2.Rows[0]["COUNT(*)"].ToString());
+                //    (Count of all procedures - Count of Planned Procedures)  / Count of All Procedures
+                //  Simply put:    (Count of procedures scheduled) / Count of All procedures          
+                returnval = ((TOTALCOUNTPROCEDURES - PROCEDURESPLANNED) / TOTALCOUNTPROCEDURES) * 100;
+                return (float) Math.Round(returnval, 2);
 
+            }
 
-            // percentage returnval
-            returnval = ((TOTALCOUNTPROCEDURES - PROCEDURESPLANNED) / TOTALCOUNTPROCEDURES) * 100;
-
-            return returnval;
         }
 
 
