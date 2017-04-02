31f5fb2e7827ff4dbc148eb5f7ff27bcbd76ed74
trigorob <trigomusic@hotmail.com>
2017-04-01 17:44:28 -0700

    minor fix with naming display

--- KPI/StretchKPICustomForm.cs
+++ KPI/StretchKPICustomForm.cs
@@ -48,7 +48,9 @@ namespace KPIReporting.KPI
                 pat.LName = raw.Rows[i]["LName"].ToString();
                 pat.FName = raw.Rows[i]["FName"].ToString();
                 pat.MiddleI = raw.Rows[i]["MiddleI"].ToString();
-                row["Name"] = pat.GetNameLF();
+                // row["Name"] = pat.GetNameLF();
+		row["Name"] = raw.Rows[i]["FName"].ToString() + " " + raw.Rows[i]["MiddleI"].ToString() +
+                 " " + raw.Rows[i]["LName"].ToString();		    
                 pat.HmPhone = raw.Rows[i]["HmPhone"].ToString();
                 pat.WkPhone = raw.Rows[i]["WkPhone"].ToString();
                 pat.WirelessPhone = raw.Rows[i]["WirelessPhone"].ToString();
@@ -110,7 +112,7 @@ namespace KPIReporting.KPI
                 pat.MiddleI = raw.Rows[i]["MiddleI"].ToString();
                 // row["Name"]=pat.GetNameLF();
                 row["Name"] = raw.Rows[i]["FName"].ToString() + " " + raw.Rows[i]["MiddleI"].ToString() +
-                 ". " + raw.Rows[i]["LName"].ToString();
+                 " " + raw.Rows[i]["LName"].ToString();
                 pat.HmPhone = raw.Rows[i]["HmPhone"].ToString();
                 pat.WkPhone = raw.Rows[i]["WkPhone"].ToString();
                 pat.WirelessPhone = raw.Rows[i]["WirelessPhone"].ToString();
