1dfa9a6cb903030a7cb708198b6872c9cfc9cc15
trigorob <trigomusic@hotmail.com>
2017-04-03 12:05:39 -0700

    kpi14 test update

--- UnitTests/KPI14 - Completed Cases.cs
+++ UnitTests/KPI14 - Completed Cases.cs
@@ -32,16 +32,50 @@ namespace UnitTests
                  Convert.ToDateTime("2016-03-20"), Convert.ToDateTime("2017-04-05"));
 
             Assert.AreEqual(0, patsFor14.Rows.Count);
-            
+
+
             var seedData2 = File.ReadAllText(@"..\..\Resources\kpi14completedcasesPT2.txt");
             DatabaseTools.ExecuteSqlScript("localhost", "3306", "root", "", seedData2);
 
             var startDate = new DateTime(2016, 3, 20);
             var endDate = new DateTime(2017, 4, 5);
-            var patsFor14a = KPICompletedCases.GetCompletedCasesPats(startDate, endDate);
+
+            String patQuery = @"
+                SELECT DISTINCT p.PatNum, p.LName, p.FName, p.MiddleI, 
+                           p.HmPhone, p.WkPhone, p.WirelessPhone, p.Email
+                FROM unittest.procedurelog pl
+                JOIN unittest.appointment a ON pl.PlannedAptNum = a.AptNum
+                JOIN unittest.procedurecode pc ON pc.CodeNum = pl.CodeNum
+                JOIN unittest.patient p ON p.PatNum = pl.PatNum
+                WHERE a.AptStatus = 6
+                AND pl.ProcStatus = 2      
+                AND (pl.ProcDate BETWEEN '2016-03-21' AND '2018-03-21' )
+            ";
+
+
+            // var patsFor14a = KPICompletedCases.GetCompletedCasesPats(startDate, endDate);
+            var patsFor14a = StretchKPICustomForm.GetPatients(startDate, endDate, patQuery);
             Assert.IsNotNull(patsFor14a);
             Assert.AreEqual(1, patsFor14a.Rows.Count);
-            
+
+            var compcasesFor14a = KPICompletedCases.GetCompletedCasesPerPat(startDate, endDate,
+                "1401");
+
+            Assert.AreEqual(1, compcasesFor14a.Rows.Count);
+
+            var seedData3 = File.ReadAllText(@"..\..\Resources\kpi14completedcasesPT3.txt");
+            DatabaseTools.ExecuteSqlScript("localhost", "3306", "root", "", seedData3);
+
+            var patsFor14b = StretchKPICustomForm.GetPatients(startDate, endDate, patQuery);
+            Assert.IsNotNull(patsFor14b);
+            Assert.AreEqual(2, patsFor14b.Rows.Count);
+
+            var compcasesFor14b = KPICompletedCases.GetCompletedCasesPerPat(startDate, endDate,
+                "1402");
+            Assert.AreEqual(3, compcasesFor14b.Rows.Count);
+            compcasesFor14b = KPICompletedCases.GetCompletedCasesPerPat(startDate, endDate,
+                "1403");
+            Assert.AreEqual(3, compcasesFor14b.Rows.Count);
 
         }
     }
